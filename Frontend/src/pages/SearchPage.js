c3430b32cbb0c15305f0ceb0a62821b6405ae303
PeterBoss <peter2137@gmail.com>
2017-05-03 15:32:15 +0200

    SearchPage stuff

--- Frontend/src/pages/SearchPage.js
+++ Frontend/src/pages/SearchPage.js
@@ -4,47 +4,20 @@ import 'react-infinite-calendar/styles.css';
 
 class SearchPage extends Component {
 
-
-
-    constructor() {
-        super();
-        this.state = {value: "1"};
-    }
-
-
-    change(event) {
-        alert(event.target.value);
-        this.setState({value: event.target.value});
-    }
-
     render() {
         return (
 
 
 
             <div>
-                <InfiniteCalendar
-                    width={200}
-                    height={300}
-                />
                 <form>
-                    <select id="month" onChange={this.change} value={this.state.value}>
-                        <option value="1">January</option>
-                        <option value="2">February</option>
-                        <option value="3">March</option>
-                        <option value="4">April</option>
-                        <option value="5">May</option>
-                        <option value="6">June</option>
-                        <option value="7">July</option>
-                        <option value="8">August</option>
-                        <option value="9">September</option>
-                        <option value="10">October</option>
-                        <option value="11">November</option>
-                        <option value="12">December</option>
-                    </select>
-                    <select>
-                        <option></option>
-                    </select>
+                    <InfiniteCalendar
+                        width={200}
+                        height={300}
+                        min={new Date()}
+                        max= {new Date(2020, 11, 31)}
+                    />
+
                     <input type="submit" value="Submit"/>
                 </form>
             </div>
